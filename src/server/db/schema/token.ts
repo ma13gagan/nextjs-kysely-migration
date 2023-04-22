8f258ff9f5b1ccfe56cdc57e1edab5c9a8c60ec6
Gagandeep Singh <gagandeep.singh.6895@protonmail.com>
2023-04-22 12:23:13 +0530

    relation fix in schema

--- src/server/db/schema/token.ts
+++ src/server/db/schema/token.ts
@@ -1,11 +1,9 @@
-import IUser from "./users"
-
 interface IToken {
 	id: string
 	type: "ACCESS" | "RESET_PASSWORD" | "VERIFY_EMAIL"
 	expires: Date
 	token: string
-	userId: IUser
+	userId: string
 	blacklisted: boolean
 }
 
